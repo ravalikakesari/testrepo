5672148e554a871559567fb36ad810eefa1dae12
ravalika <ravalika@21.com>
2020-07-15 13:11:18 +0530

    oops

---
+++ inheritance.js
@@ -0,0 +1,34 @@
+class Animal {
+
+    constructor(name) {
+      this.speed = 0;
+      this.name = name;
+    }
+  
+    run(speed) {
+      this.speed = speed;
+      console.log(`${this.name} runs with speed ${this.speed}.`);
+    }
+  
+    stop() {
+      this.speed = 0;
+      console.log(`${this.name} stands still.`);
+    }
+  
+  }
+  
+  class Rabbit extends Animal {
+    hide() {
+      console.log(`${this.name} hides!`);
+    }
+  
+    stop() {
+      super.stop(); // call parent stop
+      this.hide(); // and then hide
+    }
+  }
+  
+  let rabbit = new Rabbit("White Rabbit");
+  
+  rabbit.run(5); // White Rabbit runs with speed 5.
+  rabbit.stop(); // White Rabbit stands still. White rabbit hides!
